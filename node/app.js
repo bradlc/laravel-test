e288a4730a650c8c426fc7cf4b8220cb2e7f746b
Brad Cornes <bradlc41@gmail.com>
2017-05-22 18:16:26 +0100

    Add request test

--- node/app.js
+++ node/app.js
@@ -1,8 +1,16 @@
 var express = require('express')
+var request = require('request')
 var app = express()
 
 app.get('/node', function (req, res) {
-  res.send('hello world')
+  request('http://localhost', function (error, response, body) {
+    var data = JSON.parse(body);
+    var html = ''
+    for (var item in data) {
+      html = html + data[item].Name + '<br>'
+    }
+    res.send(html)
+  });
 })
 
 app.listen(3000)
