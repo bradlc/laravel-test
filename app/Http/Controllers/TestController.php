c97152395b0011f7f3398e03a91cb95744fd541b
Brad Cornes <bradlc41@gmail.com>
2017-05-22 16:57:00 +0100

    lowercase P

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -10,7 +10,7 @@ class TestController extends Controller
     public function test()
     {
         // return view('test');
-        $cities = City::get()->where('Population', '>', 5000000);
+        $cities = City::get()->where('population', '>', 5000000);
 
         return response()->json($cities);
     }
